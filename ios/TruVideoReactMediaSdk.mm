4f535e3755e05ca0529596f625bedbad490d485b
Himanshu <premanshu.solanki@cellulant.io>
2026-04-30 13:02:07 +0530

    stream methods added into TruvideoReactMediaSdk.mm file

--- ios/TruVideoReactMediaSdk.mm
+++ ios/TruVideoReactMediaSdk.mm
@@ -60,6 +60,67 @@ RCT_EXTERN_METHOD(search:(NSString *)tag
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
+// Search by remote ID
+RCT_EXTERN_METHOD(searchById:(NSString *)id
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+// Create stream upload request
+RCT_EXTERN_METHOD(createStreamUploadRequest:(NSString *)filePath
+                 resolve:(RCTPromiseResolveBlock)resolve
+                 reject:(RCTPromiseRejectBlock)reject)
+
+// Get all stream upload requests
+RCT_EXTERN_METHOD(getAllStreamUploadRequests:(RCTPromiseResolveBlock)resolve
+                 reject:(RCTPromiseRejectBlock)reject)
+
+// Get stream upload request by ID
+RCT_EXTERN_METHOD(getStreamUploadRequestById:(NSString *)id
+                 resolve:(RCTPromiseResolveBlock)resolve
+                 reject:(RCTPromiseRejectBlock)reject)
+
+// Upload stream upload request
+RCT_EXTERN_METHOD(uploadStreamUploadRequest:(NSString *)id
+                 title:(NSString *)title
+                 tags:(NSString *)tags
+                 metadata:(NSString *)metadata
+                 includeInReport:(BOOL)includeInReport
+                 isLibrary:(BOOL)isLibrary
+                 resolve:(RCTPromiseResolveBlock)resolve
+                 reject:(RCTPromiseRejectBlock)reject)
+
+// Pause stream upload request
+RCT_EXTERN_METHOD(pauseStreamUploadRequest:(NSString *)id
+                 resolve:(RCTPromiseResolveBlock)resolve
+                 reject:(RCTPromiseRejectBlock)reject)
+
+// Resume stream upload request
+RCT_EXTERN_METHOD(resumeStreamUploadRequest:(NSString *)id
+                 resolve:(RCTPromiseResolveBlock)resolve
+                 reject:(RCTPromiseRejectBlock)reject)
+
+// Retry stream upload request
+RCT_EXTERN_METHOD(retryStreamUploadRequest:(NSString *)id
+                 resolve:(RCTPromiseResolveBlock)resolve
+                 reject:(RCTPromiseRejectBlock)reject)
+
+// Delete stream upload request
+RCT_EXTERN_METHOD(deleteStreamUploadRequest:(NSString *)id
+                 resolve:(RCTPromiseResolveBlock)resolve
+                 reject:(RCTPromiseRejectBlock)reject)
+
+// Start streaming all upload requests (AsyncStream → JS events)
+RCT_EXTERN_METHOD(startStreamAllUploadRequests)
+
+// Stop streaming all upload requests
+RCT_EXTERN_METHOD(stopStreamAllUploadRequests)
+
+// Start streaming a single upload request by ID
+RCT_EXTERN_METHOD(startStreamUploadRequestById:(NSString *)id)
+
+// Stop streaming a single upload request by ID
+RCT_EXTERN_METHOD(stopStreamUploadRequestById:(NSString *)id)
+
 + (BOOL)requiresMainQueueSetup
 {
   return NO;
